0bb990815aa353d8780a5d7efd2a71e8556de4af
Ahmad <ahmad.k.farag@gmail.com>
2018-12-09 16:47:43 -0500

    encoding change

--- prediction/xgboost_classifier.py
+++ prediction/xgboost_classifier.py
@@ -1,21 +1,33 @@
 import pandas
 from sklearn import metrics
+from sklearn.preprocessing import OneHotEncoder
 from xgboost import XGBClassifier
 
 df = pandas.read_excel(
     "/home/ahmad/repos/region-representation-learning/chicago/Household Income by Race and Census Tract and Community Area.xlsx")
 
 # drop the header rows
-dropped = df.drop(df.index[:4])
+df.drop(df.index[:2], inplace=True)
 
-ids = dropped['TRACT']
-median = dropped['B1913001']
-# create one hot
-one_hot = pandas.get_dummies(ids, prefix="tract", drop_first=True)
+data = df[['TRACT', 'B1913001']]
 
+# data = data[pandas.notnull(df['TRACT'])]
+data = data.dropna(subset=['TRACT'])
+# create one hots
+ids = data['TRACT'].values
+ids = ids.reshape(data.shape[0], 1)
+one_hot_encoder = OneHotEncoder(sparse=False)
+encoded = one_hot_encoder.fit_transform(ids)
+
+
+print(encoded)
+
+median = data['B1913001']
+# one_hot = pandas.get_dummies(ids, , drop_first=True)
+#
 from sklearn.model_selection import train_test_split
 
-X_train, X_test, y_train, y_test = train_test_split(one_hot, median, test_size=.2, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(encoded, median, test_size=.2, random_state=0)
 
 alg = XGBClassifier(learning_rate=0.1, n_estimators=140, max_depth=5, objective='binary:logistic', nthread=4)
 
