740b30565a555026c0c14745e67bda2fcdba30dd
Ahmad <ahmad.k.farag@gmail.com>
2018-12-09 20:54:47 -0500

    created one hot file with median price

--- prediction/xgboost_classifier.py
+++ prediction/xgboost_classifier.py
@@ -1,41 +1,27 @@
-import pandas
-from sklearn.preprocessing import OneHotEncoder
+from numpy import loadtxt
+from sklearn.metrics import accuracy_score
 from xgboost import XGBClassifier
 
-df = pandas.read_excel(
-    "/home/ahmad/repos/region-representation-learning/chicago/Household Income by Race and Census Tract and Community Area.xlsx")
-
-# drop the header rows
-df.drop(df.index[:2], inplace=True)
-
-data = df[['TRACT', 'B1913001']]
-
-# drop na rows
-data = data.dropna(subset=['TRACT'])
-# create one hots
-ids = data['TRACT'].values
-ids = ids.reshape(data.shape[0], 1)
-one_hot_encoder = OneHotEncoder(sparse=False)
-encoded = one_hot_encoder.fit_transform(ids)
-
-print(encoded)
-
-median = data['B1913001']
-
+dataset = loadtxt('/home/ahmad/repos/region-representation-learning/prediction/onehot_and_median_income.csv', delimiter=",")
 from sklearn.model_selection import train_test_split
 
-X_train, X_test, y_train, y_test = train_test_split(encoded, median, test_size=.2, random_state=0)
+# split data into X and y
+X = dataset[:, 0:800]
+Y = dataset[:, 801]
 
-alg = XGBClassifier(learning_rate=0.1, n_estimators=140, max_depth=5, objective='binary:logistic', nthread=4)
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=.2, random_state=0)
+
+model = XGBClassifier(learning_rate=0.1, n_estimators=140, max_depth=5, objective='binary:logistic', nthread=4)
+# alg = XGBClassifier()
 
 print('Start Training')
-alg.fit(X_train, y_train)
+model.fit(X_train, y_train)
 
 print("Start Predicting")
 
-predictions = alg.predict(X_test)
-
-predictions
+predictions = model.predict(X_test)
+accuracy = accuracy_score(y_test, predictions)
+print("Accuracy: %.2f%%" % (accuracy * 100.0))
 # do scoring
 # print("F1 Score: %f" % metrics.f1_score(y_test, predictions))
 # print("Score: %.4g" % metrics.accuracy_score(y_test, predictions))
