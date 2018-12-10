d68995c76e09386665339695bd6d5ffb9d79840d
Ahmad <ahmad.k.farag@gmail.com>
2018-12-09 20:58:09 -0500

    created one hot file with median price added basic linear regression

--- prediction/xgboost_classifier.py
+++ prediction/xgboost_classifier.py
@@ -1,8 +1,10 @@
 from numpy import loadtxt
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import accuracy_score
 from xgboost import XGBClassifier
 
-dataset = loadtxt('/home/ahmad/repos/region-representation-learning/prediction/onehot_and_median_income.csv', delimiter=",")
+dataset = loadtxt('/home/ahmad/repos/region-representation-learning/prediction/onehot_and_median_income.csv',
+                  delimiter=",")
 from sklearn.model_selection import train_test_split
 
 # split data into X and y
@@ -12,16 +14,24 @@ Y = dataset[:, 801]
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=.2, random_state=0)
 
 model = XGBClassifier(learning_rate=0.1, n_estimators=140, max_depth=5, objective='binary:logistic', nthread=4)
-# alg = XGBClassifier()
 
 print('Start Training')
 model.fit(X_train, y_train)
+regression = LinearRegression()
+regression.fit(X_train, y_train)
+
+
 
 print("Start Predicting")
 
 predictions = model.predict(X_test)
+lin_pred = regression.predict(X_test)
+
 accuracy = accuracy_score(y_test, predictions)
-print("Accuracy: %.2f%%" % (accuracy * 100.0))
+lin_accuracy = accuracy_score(y_test, lin_pred)
+
+print(regression.score(X_test,y_test))
+print(model.score(X_test,y_test))
 # do scoring
 # print("F1 Score: %f" % metrics.f1_score(y_test, predictions))
 # print("Score: %.4g" % metrics.accuracy_score(y_test, predictions))
