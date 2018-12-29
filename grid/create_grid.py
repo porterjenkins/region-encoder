4f1f4758fb05044a9f5c6a8b3ab9596e91a14527
Ahmad Farag <ahmad.k.farag@gmail.com>
2018-12-29 01:29:16 -0500

    Add Sys Line to allow for command line execution

--- grid/create_grid.py
+++ grid/create_grid.py
@@ -1,6 +1,11 @@
+import os
 import pickle
+import sys
 
 import numpy
+
+# this should add files properly
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from config import get_config
 
