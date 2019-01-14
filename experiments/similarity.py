d6c6d10fdc65018a86d133a8fd6e1891c53bc70b
Porter Jenkins <porterjenkins@gmail.com>
2019-01-14 12:31:19 -0500

    Naive prediction model for similarity: mean of adjacent regions

--- experiments/similarity.py
+++ experiments/similarity.py
@@ -8,6 +8,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 import pandas as pd
 from sklearn.neighbors import NearestNeighbors
+from model.utils import load_embedding
 
 def get_knn(X, k, idx_coor_map):
     nbrs = NearestNeighbors(n_neighbors=k, algorithm='ball_tree')
@@ -26,16 +27,80 @@ def get_knn(X, k, idx_coor_map):
 
     return distances, indices
 
+
+def cv_adj_mean(regions, matrix_idx_map, y):
+    n = len(y)
+    errors = [[], []]
+
+    region_cntr = 0
+    for coor, r in regions.items():
+        adj_list = r.adjacent
+
+        r_mtx_idx = matrix_idx_map[coor]
+        y_test = [y[r_mtx_idx]]
+        if np.isnan(y_test):
+            continue
+        y_hat = 0
+        is_valid_y_cnt = 0
+
+        for neighbor_coor in adj_list:
+            n_mtx_id = matrix_idx_map[neighbor_coor]
+
+            if np.isnan(y[n_mtx_id]):
+                continue
+            else:
+                y_hat += y[n_mtx_id]
+                is_valid_y_cnt += 1
+
+        y_hat = [y_hat / is_valid_y_cnt]
+        mse = mean_squared_error(y_true=y_test, y_pred=y_hat)
+        mae = mean_absolute_error(y_test, y_hat)
+        errors[0].append(mse)
+        errors[1].append(mae)
+
+        region_cntr += 1
+    errors = np.transpose(np.array(errors))
+    mean_cv_err = np.round(np.mean(errors, axis=0), 4)
+    std_cv_err = np.round(np.std(errors, axis=0), 4)
+
+    return mean_cv_err[0], std_cv_err[0], mean_cv_err[1], std_cv_err[1], errors
+
+
+def cv_naive_mean(y):
+    n = len(y)
+    indices = list(range(n))
+
+    errors = np.zeros((n, 2))
+
+    for i in indices:
+        train_idx = indices[:i] + indices[(i + 1):]
+        y_train = y[train_idx]
+        y_hat = [np.mean(y_train)]
+
+        y_test = [y[i]]
+
+        mse = mean_squared_error(y_true=y_test, y_pred=y_hat)
+        mae = mean_absolute_error(y_test, y_hat)
+        errors[i, 0] = mse
+        errors[i, 1] = mae
+
+    mean_cv_err = np.round(np.mean(errors, axis=0), 4)
+    std_cv_err = np.round(np.std(errors, axis=0), 4)
+
+    return mean_cv_err[0], std_cv_err[0], mean_cv_err[1], std_cv_err[1], errors
+
 class SimilarityModel(object):
-    def __init__(self, y, weight_mtx):
+    def __init__(self, y, weight_mtx=None):
         self.y = y
         self.weight_mtx = weight_mtx
-        self.spatial_features = self.get_features(y, weight_mtx)
+        if weight_mtx is not None:
+            self.spatial_features = self.get_features(y, weight_mtx)
         self.n = y.shape[0]
 
     def get_features(self, y, W):
 
         return np.dot(y, W).reshape(-1, 1)
+
 
     def cv_ols(self):
         indices = list(range(self.n))
@@ -77,10 +142,22 @@ if __name__ == "__main__":
     region_grid.load_housing_data(c['housing_data_file'])
 
     y_house = region_grid.get_target_var("house_price")
+    results = []
+
+
+    # Adjacency Average Model
+    mse, mse_std, mae, mae_std, err_adj = cv_adj_mean(region_grid.regions, region_grid.matrix_idx_map, y_house)
+    results.append(['adjacent avg', mse, mse_std, mae, mae_std])
+
     y_is_valid = np.where(~np.isnan(y_house))[0]
     y_house = y_house[y_is_valid]
 
-    results = []
+
+
+    # Global Avg Model
+    global_avg = SimilarityModel(y_house)
+    mse, mse_std, mae, mae_std, err_euclidean = cv_naive_mean(y_house)
+    results.append(['global avg', mse, mse_std, mae, mae_std])
 
 
     # euclidean model
@@ -102,9 +179,19 @@ if __name__ == "__main__":
     mse, mse_std, mae, mae_std, err_flow = mod_flow.cv_ols()
     results.append(['flow', mse, mse_std, mae, mae_std])
 
+    # Matrix Factorization as similarity
+    nmf = load_embedding(c['nmf_file'])
+    W_nmf = np.matmul(nmf, np.transpose(nmf))
+    W_nmf = W_nmf[y_is_valid, :]
+    W_nmf = W_nmf[:, y_is_valid]
+
+    mod_nmf = SimilarityModel(y_house, W_nmf)
+    mse, mse_std, mae, mae_std, err_nmf = mod_nmf.cv_ols()
+    results.append(['matrix factorization', mse, mse_std, mae, mae_std])
+
 
     # Run with deepwalk as similarity measure
-    deepwalk = region_grid.load_embedding(c['deepwalk_file'])
+    deepwalk = load_embedding(c['deepwalk_file'])
     W_deepwalk = np.matmul(deepwalk, np.transpose(deepwalk))
     W_deepwalk = W_deepwalk[y_is_valid, :]
     W_deepwalk = W_deepwalk[:, y_is_valid]
@@ -116,7 +203,7 @@ if __name__ == "__main__":
 
 
     # Run with RegionEncoder as similarity measure
-    re_embed = region_grid.load_embedding(c['embedding_file'])
+    re_embed = load_embedding(c['embedding_file'])
     W_re = np.matmul(re_embed, np.transpose(re_embed))
     W_re = W_re[y_is_valid, :]
     W_re = W_re[:, y_is_valid]
