002363bb599c64e6cf5049a0727961277dc3f99c
Porter Jenkins <porterjenkins@gmail.com>
2019-01-29 09:29:03 -0500

    Add train method to MSNE

--- model/msne.py
+++ model/msne.py
@@ -9,6 +9,84 @@ import numpy as np
 from geopy.distance import distance
 import pickle
 from scipy import sparse
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+
+class MSNE(nn.Module):
+    def __init__(self, network_size, hidden_dim):
+        super(MSNE, self).__init__()
+        ### Encoder
+        self.W_encoder_1 = nn.Linear(network_size, 256)
+        self.W_encoder_2 = nn.Linear(256, 84)
+        self.W_encoder_3 = nn.Linear(84, hidden_dim)
+
+        ### Decoder
+        self.W_decoder_1 = nn.Linear(hidden_dim, 256)
+        self.W_decoder_2 = nn.Linear(256, network_size)
+
+
+
+
+    def forward(self, X):
+
+        # Encode
+        H = F.relu(self.W_encoder_1(X))
+        H = F.relu(self.W_encoder_2(H))
+        Z = F.relu(self.W_encoder_3(H))
+
+        # Decode
+        H = F.relu(self.W_decoder_1(Z))
+        X_reconstruction = F.relu(self.W_decoder_2(H))
+
+        return X_reconstruction
+
+
+    def get_optimizer(self, lr):
+        optimizer = optim.SGD(self.parameters(), lr=lr, momentum=0.9)
+        self.mse_loss = torch.nn.MSELoss(reduction='mean')
+
+        return optimizer
+
+
+    def loss(self, X_dist_true, X_dist_hat, X_mobility_true, X_mobility_hat):
+
+        mse_dist = self.mse_loss(X_dist_hat, X_dist_true)
+        mse_mobility = self.mse_loss(X_mobility_hat, X_mobility_true)
+
+        return mse_dist + mse_mobility
+
+
+    def run_train_job(self, X_poi_dist, X_poi_mobility, n_epochs, batch_size, lr):
+
+        n_samples = X_poi_dist.shape[0]
+        optimizer = self.get_optimizer(lr)
+
+        print("Beginning Train Job for MSNE - epochs: {}, batch size: {}, lr: {}".format(n_epochs, batch_size, lr))
+        for e in range(n_epochs):
+            permute_idx = np.random.permutation(np.arange(n_samples))
+            running_loss = 0
+            for step in range(int(n_samples / batch_size)):
+                # zero the parameter gradients
+                optimizer.zero_grad()
+                start_idx = step * batch_size
+                end_idx = start_idx + batch_size
+                batch_idx = permute_idx[start_idx:end_idx]
+
+                X_hat_dist = self.forward(X_poi_dist[batch_idx, :])
+                X_hat_mobility = self.forward(X_poi_mobility[batch_idx, :])
+
+                loss = self.loss(X_poi_dist[batch_idx, :], X_hat_dist, X_poi_mobility[batch_idx, :], X_hat_mobility)
+                loss.backward()
+
+                running_loss += loss.item()
+
+            avg_loss = running_loss/int(n_samples / batch_size)
+            print("Epoch: {} - Average Train loss: {:.4f}".format(e+1, avg_loss))
+
+
 
 def get_poi_poi_dist_mtx(region_grid, n_region, n_cat):
     X = np.zeros((n_region, n_cat ** 2))
@@ -96,9 +174,17 @@ if __name__ == "__main__":
 
         with open(c['data_dir_main'] + "mnse_poi_dist_mtx.p", 'rb') as f:
             X_poi_dist = pickle.load(f)
+            X_poi_dist = X_poi_dist.todense()
 
         with open(c['data_dir_main'] + "mnse_poi_mobility_mtx.p", 'rb') as f:
             X_poi_mobility = pickle.load(f)
+            X_poi_mobility = X_poi_mobility.todense()
 
 
+    network_size = X_poi_dist.shape[1]
+    msne = MSNE(network_size, hidden_dim=int(c['hidden_dim_size']))
 
+    X_poi_dist = torch.Tensor(X_poi_dist)
+    X_poi_mobility = torch.Tensor(X_poi_dist)
+
+    msne.run_train_job(X_poi_dist, X_poi_mobility, n_epochs=5, batch_size=50, lr=.05)
